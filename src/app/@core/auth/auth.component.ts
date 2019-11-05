b1c722fa9807eae7cd4c60c864e79be454ead2e2
vikct <18026233+vikct@users.noreply.github.com>
2019-11-05 16:13:12 +0800

    flex layout applied on login component

--- src/app/@core/auth/auth.component.ts
+++ src/app/@core/auth/auth.component.ts
@@ -3,7 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-auth',
   template: `
-    <div class="auth-container">
+    <div
+      class="auth-container"
+      fxLayout="row"
+      fxLayout.xs="column"
+      fxLayoutAlign="center center"
+      fxFlexFill
+      >
       <router-outlet></router-outlet>
     </div>
   `,
