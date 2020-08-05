930a1acb624501993d44dbe7555d43234eca57e0
victor.tan <victor.tan@monitorerp.com>
2020-08-05 20:35:53 +0800

    cont.

--- src/app/pages/dashboard/covid19/covid19.component.ts
+++ src/app/pages/dashboard/covid19/covid19.component.ts
@@ -4,7 +4,7 @@ import {  interval, timer } from 'rxjs';
 import { flatMap, groupBy, map } from 'rxjs/operators';
 
 import { Covid19Service } from './covid19.service';
-import { Country,Documentation } from './interfaces/covid19.interface';
+import { Documentation, Country, CountryStatus } from './interfaces/covid19.interface';
 
 @Component({
   selector: 'app-covid19',
@@ -20,20 +20,12 @@ export class Covid19Component implements OnInit {
   displayedColumns: string[] = ['Name', 'Description', 'Path'];
   dataSource = this.docs;
 
-  constructor(private covid19Service: Covid19Service) {
+  selectedCountry: string;
+  countryCasesChartOptions: any;
+  cases: CountryStatus[] = [];
 
-    // interval(2*60*1000)
-    // timer(0, 2*60*1000)
-    //   .pipe(
-    //     flatMap(() => this.covid19Service.getCountries()),
-    //     map(data =>
-    //       data.sort((a, b) => a.Country.localeCompare(b.Country))
-    //     )
-    //   )
-    //   // .subscribe(data => console.log(data))
-    //   .subscribe(result => {
-    //     this.countries = result
-    //   })
+  constructor(private covid19Service: Covid19Service) {
+    this.getCasesByCountry(this.selectedCountry);
   }
 
   getCountries() {
@@ -57,9 +49,10 @@ export class Covid19Component implements OnInit {
                 })
   }
 
-  getValue(country: string) {
-    // interval(2*60*1000)
-    timer(0, 2*60*1000)
+  getCasesByCountry(country: string) {
+    if (country) {
+        // interval(2*60*1000)
+      timer(0, 2*60*1000)
       .pipe(
         flatMap(() => this.covid19Service.countryDayOneRoute(country)),
         map(data =>
@@ -70,6 +63,49 @@ export class Covid19Component implements OnInit {
       // .subscribe(result => {
       //   this.countries = result
       // })
+      }
+  }
+
+  onChangeCountry(country: string) {
+    this.covid19Service.countryDayOneRoute(country).subscribe(cases => {
+      this.cases = cases;
+      this.setOptions();
+    });
+  }
+
+  setOptions() {
+    this.countryCasesChartOptions = {
+      title: {
+        text: 'COVID-19 STATUS CHART',
+      },
+      legend: {
+        data: ['Confirmed', 'Recovered', 'Deaths']
+      },
+      tooltip: {
+      },
+      xAxis: {
+        data: this.cases.map(c => new Date(c.Date).toLocaleDateString()),
+      },
+      yAxis: {
+        type: 'value'
+      },
+      series: [{
+        name: 'Confirmed',
+        type: 'line',
+        data: this.cases.map(c => c.Confirmed),
+      },
+      {
+        name: 'Recovered',
+        type: 'line',
+        data: this.cases.map(c => c.Recovered),
+      },
+      {
+        name: 'Deaths',
+        type: 'line',
+        data: this.cases.map(c => c.Deaths),
+      },
+      ]
+    };
   }
 
   ngOnInit(): void {
