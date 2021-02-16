f3309e5be0eb14157d4d8d4a3200a8e83a7d29c7
victor.tan <victor.tan@monitorerp.com>
2021-02-16 10:11:48 +0800

    update to angular 11

--- src/app/@core/transferendum/transferendum.component.spec.ts
+++ src/app/@core/transferendum/transferendum.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { TransferendumComponent } from './transferendum.component';
 
@@ -6,7 +6,7 @@ describe('TransferendumComponent', () => {
   let component: TransferendumComponent;
   let fixture: ComponentFixture<TransferendumComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ TransferendumComponent ]
     })
