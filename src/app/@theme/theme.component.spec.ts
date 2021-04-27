82b92e3b6f09c65c0a344c08192b6b3a59c77ea0
vikct <victor.tan@rentguard.com.my>
2021-04-27 20:01:31 +0800

    updated angular/core, angular/cli

--- src/app/@theme/theme.component.spec.ts
+++ src/app/@theme/theme.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ThemeComponent } from './theme.component';
 
@@ -6,7 +6,7 @@ describe('ThemeComponent', () => {
   let component: ThemeComponent;
   let fixture: ComponentFixture<ThemeComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ThemeComponent ]
     })
